d9e8cd2e81c6f2f5a9de7b07b11d8b0e0faa474d
Jashan337 <jashanmahtani@gmail.com>
2026-05-14 21:50:43 +0700

    update rootPath 3rd

--- js/navbar.js
+++ js/navbar.js
@@ -8,7 +8,7 @@ class MyNavbar extends HTMLElement {
 
         if (isGitHub) {
             // Change 'your-repo-name' to your actual GitHub repository name
-            this.rootPath = '/'; 
+            this.rootPath = 'ntpcuber/'; 
         } else if (isLocalServer) {
             // Live Server treats the project folder as root
             this.rootPath = '/'; 
